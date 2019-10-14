f04682232936b06a28508f0ac7382df83334f06a
unknown <oluwatolablessing@gmail.com>
2019-10-14 22:51:45 +0100

    control testing done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Control from "./Controls";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Control />);
+});
+
+describe("Display component", () => {
+  test("matches the snapshot!", () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
+});
+
+test("shows gate locked or not", () => {
+  expect(wrapper.queryByText(/Lock Gate/i)).toBeInTheDocument();
+});
